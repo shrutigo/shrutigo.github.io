5a9026d59bcb30f54bbbc5908b45265b10640c8f
srhegdegadikai <srhegdegadikai@gmail.com>
2020-09-07 13:39:31 -0700

    removing build.R script

--- build.R
+++
@@ -1,7 +0,0 @@
-pkgs <- c('vitae', 'tidyverse', 'lubridate', 'here', 'readxl', 'glue', 'pdftools', 'ggplot2', 'devtools', 'rmarkdown')
-install.packages(pkgs)
-
-devtools::install_github("laresbernardo/lares")
-
-rmarkdown::render('shruti-hegde/shruti-hegde.Rmd')
-rmarkdown::render_site(encoding = 'UTF-8')
